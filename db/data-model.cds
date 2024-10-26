c9352a37aff80687faa723daed2845da77454d79
gbasaldua <guillermobasalt@gmail.com>
2024-10-25 22:00:20 -0300

    add INSERT and READ superheroe

--- db/data-model.cds
+++ db/data-model.cds
@@ -5,13 +5,14 @@ using {
 
 namespace gbasdeveloper;
 
-entity superheroe : cuid, managed {
-    nombreSuperheroe : String(100);
-    nombre           : String(100);
-    franquicia       : String(20);
-    telefono         : String(9);
-    edad             : String(3);
-    fuerza           : String(3);
-    destreza         : String(3);
-    intelecto        : String(3);
+entity superheroe : managed {
+    key id               : String;
+        nombreSuperheroe : String(100);
+        nombre           : String(100);
+        franquicia       : String(20);
+        telefono         : String(9);
+        edad             : String(3);
+        fuerza           : String(3);
+        destreza         : String(3);
+        intelecto        : String(3);
 }
--- srv/CatalogService.js
+++ srv/CatalogService.js
@@ -1,6 +1,7 @@
 const cds = require("@sap/cds");
 const MongoClient = require("mongodb").MongoClient;
 const dotenv = require("dotenv");
+const { ObjectId } = require("mongodb");
 
 dotenv.config();
 
@@ -9,11 +10,68 @@ const sDbName = process.env.DATABASE_NAME;
 const client = new MongoClient(sMongoUrl);
 
 async function _crearSuperheroe(req) {
+
+
+    if (req.data.franquicia != 'Marvel' && req.data.franquicia != 'DC') {
+        req.error({ code: "401", message: "Franquisia no valida" });
+    }
+
+    await client.connect();
+    var dataBase = await client.db(sDbName);
+    var superheroes = await dataBase.collection("SuperHeroes");
+    var result = await superheroes.insertOne(req.data);
+
+    if (result.insertedId) {
+        req.data.id = result.insertedId;
+    }
+
     return req.data;
 }
 
 async function _recuperarSuperheroes(req) {
-    return req.data;
+
+    await client.connect();
+    var dataBase = await client.db(sDbName);
+    var superheroes = await dataBase.collection("SuperHeroes");
+
+    var iLimit, iOffset, oFilter;
+
+    // &top y &skip
+    console.log(req.query.SELECT.limit);
+    if (req.query.SELECT.limit) {
+        iLimit = req.query.SELECT.limit.rows.val;
+
+        if (req.query.SELECT.limit.offset) {
+            iOffset = req.query.SELECT.limit.offset.val;
+        } else {
+            iOffset = 0;
+        }
+
+
+    } else {
+        iLimit = 9999;
+        iOffset = 0;
+    }
+
+    // Select Entry
+    console.log(req.query.SELECT.one);
+
+    if (req.query.SELECT.one) {
+
+        var sId = req.query.SELECT.from.ref[0].where[2].val;
+        oFilter = {
+            "_id": new ObjectId(sId)
+        };
+    }
+
+    var results = await superheroes.find(oFilter).limit(iLimit + iOffset).toArray();
+
+    for (var i = 0; i < results.length; i++) {
+        results[i].id = results[i]._id.toString();
+    }
+
+    results = results.slice(iOffset);
+    return results;
 }
 
 async function _actualizarSuperheroe(req) {
@@ -26,7 +84,7 @@ async function _borrarSuperheroe(req) {
 
 
 module.exports = cds.service.impl(function () {
-    
+
     const { superheroe } = this.entities;
     this.on("INSERT", superheroe, _crearSuperheroe);
     this.on("READ", superheroe, _recuperarSuperheroes);
